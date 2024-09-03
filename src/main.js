b31d9ab3cf96415e8dfab224904a7cb8f482f170
NotSamfa <samfalla31@gmail.com>
2024-09-03 14:11:04 -0400

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import '../styles/normalize.css'
+import '../styles/style.css'
+import '../styles/landing-components/nav.css'
+import '../styles/landing-components/mobile-nav.css'
+import '../styles/landing-components/hero.css'
+import '../styles/landing-components/about.css'
+import '../styles/landing-components/steps.css'
+import '../styles/landing-components/footer.css'
+import '../styles/utils.css'
+
+
+import mobileNav from './utils/mobile-nav'
+
+mobileNav();
