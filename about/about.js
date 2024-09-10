cb879999763d3d7e9dfde4725430aec54a93f1f7
NotSamfa <samfalla31@gmail.com>
2024-09-10 16:24:22 -0400

    Organize files into respective initial structure

--- about/about.js
+++
@@ -1,4 +0,0 @@
-import '../styles/normalize.css'
-import '../styles/style.css'
-
-import '../styles/utils.css'
---
+++ src/utils/dashboard/movie.js
@@ -0,0 +1,29 @@
+export async function fetchMoviesData() {
+    try {
+        const response = await fetch('../src/utils/movies.json');
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching movies data:', error);
+    }
+}
+
+export function displayMovies(moviesData) {
+    const moviesContainer = document.getElementById('movies');
+    moviesContainer.innerHTML = '';
+
+    moviesData.movies.forEach(movie => {
+        const movieCard = document.createElement('div');
+        movieCard.classList.add('movie__card');
+
+        movieCard.innerHTML = `
+        <div class="movie__title">${movie.title}</div>
+        <img src="${movie.poster}" alt="${movie.title}" class="movie__poster">
+        <div class="movie__info">
+            <strong>Genre:</strong> ${movie.genre} <br>
+            <strong>Year:</strong> ${movie.release_year} <br>
+            <strong>Director:</strong> ${movie.director} <br>
+        `;
+        moviesContainer.appendChild(movieCard);
+    });
+}
