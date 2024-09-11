a0e68b2f44085e9910614a7fa4c260721ba8a436
NotSamfa <samfalla31@gmail.com>
2024-09-11 00:59:43 -0400

    Add fetch from JSON data for profile

--- profile/profile.js
+++ profile/profile.js
@@ -6,5 +6,20 @@ import '../styles/profile-components/nav.css';
 import '../styles/profile-components/main.css';
 import '../styles/landing-components/hero.css';
 
+import { displaySongs, displayMovies, fetchSongsData, fetchMoviesData } from '../src/utils/profile/history.js';
 
+
+document.addEventListener('DOMContentLoaded', async function () {
+    const songsData = await fetchSongsData();
+    const movieData = await fetchMoviesData();
+    console.log(songsData.songs);
+
+    if (songsData) {
+        displaySongs(songsData);
+    }
+
+    if (movieData) {
+        displayMovies(movieData);
+    }
+});
 
