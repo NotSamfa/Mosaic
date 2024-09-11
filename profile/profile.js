4d9a912ea21eec8e29afd4e20dfef934afd59102
DavidAlejandroMedina <davidalejo20010@gmail.com>
2024-09-10 23:45:18 -0500

    Logo Change. Add profile nav and redirects

--- profile/profile.js
+++ profile/profile.js
@@ -1,7 +1,10 @@
-import '../styles/normalize.css'
-import '../styles/style.css'
-import '../styles/landing-components/nav.css'
-import '../styles/utils.css'
+import '../styles/normalize.css';
+import '../styles/style.css';
+import '../styles/landing-components/nav.css';
+import '../styles/utils.css';
+import '../styles/profile-components/nav.css';
+import '../styles/profile-components/main.css';
+import '../styles/landing-components/hero.css';
 
 
 
